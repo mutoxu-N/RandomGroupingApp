91b2f126ea6d67b9962c30fbfb4e074c4b827015
mutoxu=N <55544957+mutoxu-N@users.noreply.github.com>
2023-12-12 17:58:09 +0900

    MainActivityのUI設定

--- app/src/main/java/com/github/mutoxu_n/randomgroupingapp/MainActivityViewModel.kt
+++ app/src/main/java/com/github/mutoxu_n/randomgroupingapp/MainActivityViewModel.kt
@@ -1,6 +1,20 @@
 package com.github.mutoxu_n.randomgroupingapp
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainActivityViewModel: ViewModel() {
+    private var _group1: List<Player> = listOf()
+    val group1 get() = _group1
+
+    private var _group2: List<Player> = listOf()
+    val group2 get() = _group2
+
+    private val _updateCount: MutableLiveData<Int> = MutableLiveData(0)
+    val updateCount: LiveData<Int> get() = _updateCount
+
+    fun roll() {
+        _updateCount.value = _updateCount.value!! + 1
+    }
 }
